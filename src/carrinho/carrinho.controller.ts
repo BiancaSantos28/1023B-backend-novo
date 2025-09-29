eca322c6f09aed70ea217fa7d6c903f53549603f
Bianca Santos <bianca.paula@estudante.ifms.edu.br>
2025-09-29 09:31:25 -0300

    Fazendo tarefa 29/09

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import produtosController from "../produtos/produtos.controller.js";
 
 interface ItemCarrinho {
     produtoId: string;
@@ -17,22 +18,49 @@ class CarrinhoController {
     //adicionarItem
     async adicionarItem(req:Request, res:Response) {
         const { usuarioId, produtoId, quantidade } = req.body;
-    } 
         //Buscar o produto no banco de dados
-        //Pegar o preço do produto
+        try{
+            const produto = await produtosController.findById(produtoId);
+            if(!produto){
+                return res.status(404).json({message: "Produto não encontrado"});
+            }
+
+           //Pegar o preço do produto
+        const precoUnitario = produto.preco;
+        const nome = produto.nome; 
+        
         //Pegar o nome do produto
-
-
+        let nome = produto.nome;
+        
         // Verificar se um carrinho com o usuário já existe
-
+        const carrinhoExistente = await CarrinhoController.findOne({usuarioId});
         // Se não existir deve criar um novo carrinho
-
         // Se existir, deve adicionar o item ao carrinho existente
-
+        if(!carrinhoExistente){
+            const novoCarrinho: Carrinho = {
+                usuarioId,
+                itens: [{   produtoId, quantidade, precoUnitario, nome}],
+                dataAtualizacao: new Date(),
+                total: precoUnitario * quantidade,
+            };  
+        } else {
+            const itemExistente = carrinhoExistente.itens.find(item => item.produtoId === produtoId);
+            if(itemExistente){
+                itemExistente.quantidade += quantidade;
+            } else {
+                carrinhoExistente.itens.push({produtoId, quantidade, precoUnitario, nome});
+            }  
+        
         // Calcular o total do carrinho
 
         // Atualizar a data de atualização do carrinho
 
+        }
+
+    } 
+
+        
+       
 
 
 
