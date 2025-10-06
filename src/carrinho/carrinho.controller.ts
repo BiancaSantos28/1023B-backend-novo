b62938f3df8e738efc9fa255bff56fcf677b3cfc
Bianca Santos <bianca.paula@estudante.ifms.edu.br>
2025-10-06 08:49:39 -0300

    enviei a atividade no moodle 06/10

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import produtosController from "../produtos/produtos.controller.js";
+import { ObjectId } from "bson";
+import { db } from "../database/banco-mongo.js";
 
 interface ItemCarrinho {
     produtoId: string;
@@ -17,50 +18,50 @@ interface Carrinho {
 class CarrinhoController {
     //adicionarItem
     async adicionarItem(req:Request, res:Response) {
+        console.log("Chegou na rota de adicionar item ao carrinho");
         const { usuarioId, produtoId, quantidade } = req.body;
-        //Buscar o produto no banco de dados
-        try{
-            const produto = await produtosController.findById(produtoId);
-            if(!produto){
-                return res.status(404).json({message: "Produto não encontrado"});
-            }
-
-           //Pegar o preço do produto
-        const precoUnitario = produto.preco;
-        const nome = produto.nome; 
-        
+         //Buscar o produto no banco de dados
+        const produto = await db.collection("produtos").findOne({ _id: ObjectId.createFromHexString(produtoId)});
+        if (!produto) {
+            return res.status(400).json({ message: "Produto não encontrado" });
+        }
+        //Pegar o preço do produto
         //Pegar o nome do produto
-        let nome = produto.nome;
-        
+        const precoUnitario = produto.preco; 
+        const nome = produto.nome; 
         // Verificar se um carrinho com o usuário já existe
-        const carrinhoExistente = await CarrinhoController.findOne({usuarioId});
-        // Se não existir deve criar um novo carrinho
-        // Se existir, deve adicionar o item ao carrinho existente
-        if(!carrinhoExistente){
+        const carrinhoExistente = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId });
+        if (!carrinhoExistente) {               
+
+            // Se não existir, criar um novo carrinho
             const novoCarrinho: Carrinho = {
                 usuarioId,
-                itens: [{   produtoId, quantidade, precoUnitario, nome}],
+                itens: [{ produtoId, quantidade, precoUnitario, nome }],
                 dataAtualizacao: new Date(),
-                total: precoUnitario * quantidade,
-            };  
-        } else {
-            const itemExistente = carrinhoExistente.itens.find(item => item.produtoId === produtoId);
-            if(itemExistente){
-                itemExistente.quantidade += quantidade;
-            } else {
-                carrinhoExistente.itens.push({produtoId, quantidade, precoUnitario, nome});
-            }  
+                total: quantidade * precoUnitario,      
+            };
+        // Se não existir deve criar um novo carrinho
         
+        // Se existir, deve adicionar o item ao carrinho existente
+
         // Calcular o total do carrinho
 
         // Atualizar a data de atualização do carrinho
 
-        }
 
-    } 
+        res.status(200).json({ message: "Item adicionado ao carrinho com sucesso" });
+        
 
         
+        }
+        
+
+    } 
        
+        
+
+
+
 
 
 
